d8ea882e855bbac6d594e76191910f73a758bb95
bum0w0 <kgy5988@naver.com>
2025-08-29 01:47:05 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/goormthon/univ/simhae/SimhaeApplication.java
@@ -0,0 +1,13 @@
+package com.goormthon.univ.simhae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimhaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimhaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goormthon/univ/simhae/SimhaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.univ.simhae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimhaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
