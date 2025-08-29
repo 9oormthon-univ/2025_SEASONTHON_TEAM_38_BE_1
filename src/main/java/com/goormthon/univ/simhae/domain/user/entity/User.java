a2848c4a4e0b4eb8c9f48f1a5f4bf01bd5f306ec
seonu <ysw130866@naver.com>
2025-08-30 01:26:13 +0900

    #2 feat: User 엔티티 구현

---
+++ src/main/java/com/goormthon/univ/simhae/domain/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.goormthon.univ.simhae.domain.user.entity;
+
+
+import com.goormthon.univ.simhae.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+}
