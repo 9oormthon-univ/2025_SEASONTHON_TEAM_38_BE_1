14b7c5bbb650f3bce43d8a4b65db7f2571bf24b7
seonu <ysw130866@naver.com>
2025-08-30 01:26:42 +0900

    #2 feat: Dream 엔티티 구현

---
+++ src/main/java/com/goormthon/univ/simhae/domain/dream/entity/Dream.java
@@ -0,0 +1,54 @@
+package com.goormthon.univ.simhae.domain.dream.entity;
+
+
+import com.goormthon.univ.simhae.domain.dream.entity.value.Category;
+import com.goormthon.univ.simhae.domain.user.entity.User;
+import com.goormthon.univ.simhae.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "dream")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Dream extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+
+    private String emoji;
+
+    @Column(columnDefinition = "TEXT")
+    private String summary;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(name = "dream_date")
+    private LocalDate dreamDate;
+
+    @Column(columnDefinition = "TEXT")
+    private String interpretation;
+
+    @Column(columnDefinition = "TEXT")
+    private String suggestion;
+
+}
