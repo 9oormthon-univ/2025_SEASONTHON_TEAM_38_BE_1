a5528ee77cc20d573170e18c014273ecfb70c876
seonu <ysw130866@naver.com>
2025-08-30 17:35:37 +0900

    #2 refactor: User 엔티티 수정

--- src/main/java/com/goormthon/univ/simhae/domain/user/entity/User.java
+++ src/main/java/com/goormthon/univ/simhae/domain/user/entity/User.java
@@ -3,13 +3,14 @@ package com.goormthon.univ.simhae.domain.user.entity;
 
 import com.goormthon.univ.simhae.global.domain.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,9 +20,11 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
+    @NotNull
     private Long id;
 
     @Column(name = "name", length = 255, nullable = false)
+    @NotNull
     private String name;
 
 }
